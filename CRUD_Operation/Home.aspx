f03968011d2fb0f9bff8ff7f1b22494f475a8941
Pravin Mane <141111800+Pravin7639@users.noreply.github.com>
2024-04-08 23:49:10 +0530

    CRUD Operation Using ASP.NET and ADO.NET

---
+++ CRUD_Operation/Home.aspx
@@ -0,0 +1,91 @@
+﻿
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Home.aspx.cs"
+    Inherits="CRUD_Operation.Home" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 100%;
+        }
+        .auto-style2 {
+            width: 123px;
+        }
+        .auto-style3 {
+            width: 285px;
+        }
+        .auto-style4 {
+            height: 23px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+
+            <table class="auto-style1">
+                <tr>
+                    <td class="auto-style2">Id</td>
+                    <td>
+                        <asp:TextBox ID="txtId" runat="server" Width="219px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Name</td>
+                    <td>
+                        <asp:TextBox ID="txtName" runat="server" Width="219px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style2">Price</td>
+                    <td>
+                        <asp:TextBox ID="txtPrice" runat="server" Width="219px"></asp:TextBox>
+                    </td>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+
+
+
+
+
+        </div>
+        <table class="auto-style1">
+            <tr>
+                <td class="auto-style3">
+                   
+                    <asp:Button ID="btnInsert" runat="server" Height="29px"  Text="Insert"  CssClass="bg-blue" BackColor="#0099FF" BorderStyle="Solid" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="btnInsert_Click" Width="63px" />
+                    
+                    <asp:Button ID="btnRead" runat="server" Text="Read" Height="29px"  CssClass="bg-blue" BackColor="#0099FF" BorderStyle="Solid" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="btnRead_Click"/>
+
+                    <asp:Button ID="btnUpdate" runat="server" Text="Update" Height="29px"  CssClass="bg-blue" BackColor="#0099FF" BorderStyle="Solid" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="btnUpdate_Click"/>
+
+                    <asp:Button ID="btnClear" runat="server" Text="Clear" Height="29px"  CssClass="bg-blue" BackColor="#0099FF" BorderStyle="Solid" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="btnClear_Click"/>
+
+                    <asp:Button ID="btnDelete" runat="server" Text="Delete" Height="29px"  CssClass="bg-blue" BackColor="#0099FF" BorderStyle="Solid" Font-Bold="True" Font-Size="Medium" ForeColor="White" OnClick="btnDelete_Click" Width="69px" />
+
+                </td>
+              
+            </tr>
+            
+
+             <tr>
+                <td class="auto-style3">
+                    <asp:GridView ID="gvProduct" runat="server" Width="449px">
+                    </asp:GridView>
+                </td>
+               
+            </tr>
+
+        </table>
+
+        <asp:Label ID="lblMessage" runat="server" Text="Label" ForeColor="Red"></asp:Label>
+
+    </form>
+</body>
+</html>
---
+++ CRUD_Operation/Home.aspx.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace CRUD_Operation
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            // Response.Write("Page_Load      <br/>");
+
+            if (!IsPostBack)
+            {
+                DataLoad();
+            }
+
+            // DataLoad();
+
+
+
+        }
+
+        void DataLoad()
+        {
+            string ConnectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;";
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            string cmdText = "Select * from Product";
+            SqlCommand cmd = new SqlCommand(cmdText, con);
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            //if (reader.HasRows)
+            //{
+            //    while (reader.Read())
+            //    {
+            //        Product p = new Product();
+            //        p.Id = (int) reader["Id"];
+            //        p.Name = reader["Name"].ToString();
+            //        p.Price = (int)reader["Price"];
+
+            //    }
+            //}
+
+
+
+
+            gvProduct.DataSource = reader;
+            gvProduct.DataBind();
+
+            con.Close();
+
+        }
+
+
+
+        protected void Page_Init(object sender, EventArgs e)
+        {
+           // Response.Write("Page_Init    <br/>");
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+           // Response.Write("Page_PreRender     <br/>");
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+
+            //to read information from textbox
+            int Id = int.Parse(txtId.Text);
+            string Name = txtName.Text;
+            int Price = int.Parse(txtPrice.Text);
+
+
+            string ConnectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;";
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            string cmdText = $"Insert into Product values ( {Id}, '{Name}', {Price} )";
+            SqlCommand cmd = new SqlCommand(cmdText, con);
+
+            con.Open();
+            int reader = cmd.ExecuteNonQuery();
+
+             
+
+            if (reader > 0)
+            {
+                lblMessage.Text = "Product Inserted Successfully";
+            }
+            else
+            {
+                lblMessage.Text = "Product Not Inserted ";
+            }
+
+           
+            con.Close();
+
+
+            DataLoad();
+
+
+
+        }
+
+        protected void btnRead_Click(object sender, EventArgs e)
+        {
+            int Id = int.Parse(txtId.Text);
+            string ConnectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;";
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            string cmdText = $"Select Name, Price from Product where @Id=Id";
+            SqlCommand cmd = new SqlCommand(cmdText, con);
+            cmd.Parameters.AddWithValue("@Id", Id);
+
+            con.Open();
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            if (reader.Read())
+            {
+                txtName.Text = reader["Name"].ToString();
+                txtPrice.Text = reader["Price"].ToString();
+            }
+
+            DataLoad();
+
+
+           
+
+
+             con.Close();
+
+
+
+        }
+
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            int Id = int.Parse(txtId.Text);
+            string Name = txtName.Text;
+            int Price = int.Parse(txtPrice.Text);
+
+
+            string ConnectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;";
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            string cmdText = $"Update Product set Name=@Name, Price=@Price where Id=@Id";
+            SqlCommand cmd = new SqlCommand(cmdText, con);
+            cmd.Parameters.AddWithValue("@Id", Id);
+            cmd.Parameters.AddWithValue("@Name", Name);
+            cmd.Parameters.AddWithValue("@Price", Price);
+
+
+            con.Open();
+            int reader = cmd.ExecuteNonQuery();
+
+            if (reader > 0)
+            {
+                lblMessage.Text = "Product Updated Successfully";
+            }
+            else
+            {
+                lblMessage.Text = "Product Not Updated ";
+            }
+
+
+
+            DataLoad();
+
+            con.Close();
+
+
+
+        }
+
+        protected void btnClear_Click(object sender, EventArgs e)
+        {
+            ClearForm();
+        }
+
+        void ClearForm()
+        {
+            txtId.Text = string.Empty;
+            txtName.Text = string.Empty;
+            txtPrice.Text = string.Empty;
+        }
+
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            int Id = int.Parse(txtId.Text);
+           // string Name = txtName.Text;
+           // int Price = int.Parse(txtPrice.Text);
+
+
+            string ConnectionString = "server=.\\sqlexpress;database=B22ADONETDB;integrated security=true;";
+            SqlConnection con = new SqlConnection(ConnectionString);
+
+            string cmdText = $"Delete Product where Id=@Id";
+            SqlCommand cmd = new SqlCommand(cmdText, con);
+            cmd.Parameters.AddWithValue("@Id", Id);
+            //cmd.Parameters.AddWithValue("@Name", Name);
+           // cmd.Parameters.AddWithValue("@Price", Price);
+
+
+            con.Open();
+            int reader = cmd.ExecuteNonQuery();
+
+            if (reader > 0)
+            {
+                lblMessage.Text = "Product Deleted Successfully";
+            }
+            else
+            {
+                lblMessage.Text = "Product Not Deleted ";
+            }
+
+
+
+            DataLoad();
+
+            con.Close();
+
+        }
+
+
+    }
+}
